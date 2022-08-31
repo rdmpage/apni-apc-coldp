ef5b45566745bb5fd235ec992cc5bdbe59aed65a
Roderic Page <rdmpage@gmail.com>
2022-08-31 08:59:53 +0100

    Code to import data

---
+++ code/import-apni.php
@@ -0,0 +1,12 @@
+<?php
+
+// Create a SQLite database from a BHL file
+
+require_once(dirname(__FILE__) . '/sqlite.php');
+
+$pdo 		= new PDO('sqlite:apni.db');    // name of SQLite database (a file on disk)
+$csv_path 	= "../data/APNI-names-2022-08-30-3047.csv";  // name of csv file to import
+
+import_csv_to_sqlite($pdo, $csv_path, $options = array());
+
+?>
